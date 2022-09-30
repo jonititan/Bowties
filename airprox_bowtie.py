d50a6bbb915e32665432aeed3bb21a7b4950c178
Joni Pelham <jonititan@gmail.com>
2022-09-30 15:30:38 +0100

    added bowtie model

--- airprox_bowtie.py
+++ airprox_bowtie.py
@@ -22,44 +22,52 @@ aesara.config.change_flags(exception_verbosity="high",optimizer=None)
 nb_samples=2000
 with bt.bowtie() as airprox:
     #variables
-
+    # EventSignal = pm.ConstantData('True',1)
+    # NoEventSignal = pm.ConstantData('False',0)
     a = pm.Normal("a", mu=0.0, sigma=5.0)
     b = pm.Normal("b", mu=0.0, sigma=1.0)
-    # a = pm.Normal("a", mu=0.0, sigma=5.0)
-    # b = pm.Normal("b", mu=0.0, sigma=1.0)
-    # a = pm.Normal("a", mu=0.0, sigma=5.0)
-    # b = pm.Normal("b", mu=0.0, sigma=1.0)
-    # a = pm.Normal("a", mu=0.0, sigma=5.0)
-    # b = pm.Normal("b", mu=0.0, sigma=1.0)
+    c = pm.Normal("c", mu=0.0, sigma=5.0)
+    d = pm.Normal("d", mu=0.0, sigma=1.0)
+    e = pm.Normal("e", mu=0.0, sigma=5.0)
+    f = pm.Normal("f", mu=0.0, sigma=5.0)
+    g = pm.Normal("g", mu=0.0, sigma=5.0)
     #Causes
-    cause1 = pm.Deterministic('Cause: Bad Luck',bt.cause(a,a))
-    # cause1 = pm.Deterministic('Bad Luck',bt.cause(a,a))
+    cause1 = pm.Deterministic('Cause\nBad Luck',bt.cause(a))
+    cause2 = pm.Deterministic('Cause\nNo Flight Plan',bt.cause(b))
         #binary 
 
     #Preventory Barriers
-    barrier1 = pm.Deterministic('Barrier: Flight Plan',bt.barrier(a,cause1))
-    barrier2 = pm.Deterministic('Barrier: ATC Warning',bt.barrier(a,barrier1))
+    barrier1 = pm.Deterministic('Barrier\nFlight Plan',bt.barrier(c,cause1))
+    barrier2 = pm.Deterministic('Barrier\nATC Warning',bt.barrier(d,bt.combine((cause2,barrier1))))
     #Top Event
-    te = pm.Deterministic('Top Event: Imminent loss of seperation',bt.topevent(barrier2))
+    te = pm.Deterministic('Top Event\nImminent loss of seperation',bt.topevent(barrier2))
 
     #Mitigation Barriers
-    barrier3 = pm.Deterministic('Barrier: Situational Awareness',bt.barrier(b,te))
+    barrier3 = pm.Deterministic('Barrier\nSituational Awareness',bt.barrier(e,te))
 
     #Consequences
-    consequence1 = pm.Deterministic('Consequence: Collision',bt.consequence(a,barrier3))
-    consequence2 = pm.Deterministic('Consequence: Avoiding Action',bt.consequence(b,barrier3))
+    consequence1 = pm.Deterministic('Consequence\nCollision',bt.consequence(f,barrier3))
+    consequence2 = pm.Deterministic('Consequence\nAvoiding Action',bt.consequence(g,barrier3))
 
     trace = pm.sample(draws=nb_samples, random_seed=1000)
 
-mcgraph = pm.model_to_graphviz(airprox)
+bowtieplot = bt.plot_all_elements(airprox)
+bowtieplot.view()
+bowtienx = bt.plot_bowtie(airprox)
+bowtienx.view()
 
-mcgraph.view()
+
+
+
+
+
 model_nodes = list(airprox.named_vars.keys())
+
 az.rcParams["plot.max_subplots"]=100
 az.plot_trace(trace,model_nodes)
 plt.title('Airprox Nodes')
 plt.tight_layout()
 plt.show()
 
-# import code
-# code.interact(local=locals())
+import code
+code.interact(local=locals())
--- bowties.py
+++ bowties.py
@@ -1,5 +1,5 @@
 import pymc as pm
-
+import networkx as nx
 
 
 class bowtie(pm.Model): #extend pymc model
@@ -31,13 +31,35 @@ class bowtie(pm.Model): #extend pymc model
         self.mitigationbarriers=[]
         self.escalatoryfactors=[]
         self.variables=[]
-   
+
+def nx_to_gv(nx_graph):
+    import graphviz
+    graph = graphviz.Digraph(nx_graph.name)
+    for node in nx_graph.nodes(data=True):
+        graph.node(node[0],**node[1])
+    graph.edges(nx_graph.edges())
+    return graph
+
+def plot_all_elements(model):
+    mcgraph = pm.model_to_graphviz(model)
+    mcgraph.attr(rankdir='LR')
+    return mcgraph
+
+def plot_bowtie(model):
+    nxgraph = pm.model_to_networkx(model)
+    remove = [node[0] for node in nxgraph.nodes(data=True) if node[1]['shape']=='ellipse']
+    nxgraph.remove_nodes_from(remove)
+    mcgraph = nx_to_gv(nxgraph)
+    mcgraph.attr(rankdir='LR')
+    return mcgraph
+
+def combine(inputList):
+    return pm.math.switch(pm.math.sum(inputList) >= 1,1,0)
 
 
-
-
-def barrier(condition,input,threshold=0):
+def barrier(condition,input,threshold=1):
     return pm.math.switch(condition > threshold,input,0)
+  
 
 def topevent(input):
     return input
@@ -45,8 +67,8 @@ def topevent(input):
 def factor(condition,input,threshold=0):
     return pm.math.switch(condition > threshold,input,0)
 
-def cause(condition,input,threshold=0):
+def cause(condition,input=1,threshold=0):
     return pm.math.switch(condition > threshold,input,0)
 
-def consequence(condition,input,threshold=0):
+def consequence(condition,input=1,threshold=0):
     return pm.math.switch(condition > threshold,input,0)
